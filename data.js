5e7e606efe783778166a64d6f4ee0a3a3ac8d38d
kalapui36 <kalapui36@gmail.com>
2026-05-08 08:50:36 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,80 @@
+// data.js
+const manualData = [
+  {
+    id: 1,
+    title: "업무포털 기본 안내",
+    description: "서울특별시교육청 업무포털 첫 화면 접속 방법 및 주요 메뉴 안내",
+    thumbnail: "photo01.png",
+    link: "manual_01.html"
+  },
+  {
+    id: 2,
+    title: "나이스(NEIS) 조퇴 상신",
+    description: "나이스 시스템에서 조퇴 상신을 하고 결재선을 지정하는 기본 방법",
+    thumbnail: "photo02.png",
+    link: "manual_02.html"
+  },
+  {
+    id: 3,
+    title: "나이스(NEIS) 출장 상신",
+    description: "관내 및 관외 출장 상신, 여비 지급 여부 체크 및 결재 요청 방법",
+    thumbnail: "photo03.png",
+    link: "manual_03.html"
+  },
+  {
+    id: 4,
+    title: "EVPN 신청 및 접속",
+    description: "외부에서 업무포털에 접속하기 위한 EVPN 사전 신청 및 실제 접속 절차",
+    thumbnail: "photo04.png",
+    link: "manual_04.html"
+  },
+  {
+    id: 5,
+    title: "내부메일 확인 및 작성",
+    description: "에듀파인 내부메일 수신함 확인, 수신자 검색 및 첨부파일을 포함한 메일 작성법",
+    thumbnail: "photo05.png",
+    link: "manual_05.html"
+  },
+  {
+    id: 6,
+    title: "문서관리카드 화면 가로로 보기",
+    description: "결재선과 본문을 한 화면에서 보기 편하게 개인문서환경을 가로로 설정하는 팁",
+    thumbnail: "photo06.png",
+    link: "manual_06.html"
+  },
+  {
+    id: 7,
+    title: "기안문 작성 기초",
+    description: "에듀파인 일반기안문 서식을 열어 본문을 작성하고 결재자를 지정하는 과정",
+    thumbnail: "photo07.png",
+    link: "manual_07.html"
+  },
+  {
+    id: 8,
+    title: "공문서 작성법 기본 원칙",
+    description: "1., 가., 1) 등 공문서 항목 기호 순서 및 들여쓰기 기본 표기 방법",
+    thumbnail: "photo08.png",
+    link: "manual_08.html"
+  },
+  {
+    id: 9,
+    title: "외부 수신자 지정",
+    description: "타 기관 발송 시 조직도 및 행안부유통을 통해 외부 수신처를 지정하는 방법",
+    thumbnail: "photo09.png",
+    link: "manual_09.html"
+  },
+  {
+    id: 10,
+    title: "지출 품의 작성",
+    description: "에듀파인에서 지출 품의 목록을 작성하고 요구 금액을 입력하여 상신하는 방법",
+    thumbnail: "photo10.png",
+    link: "manual_10.html"
+  },
+  {
+    id: 11,
+    title: "이전자료 재작성 (복사기안)",
+    description: "이전에 결재 완료된 문서의 양식을 그대로 불러와 내용을 수정하여 기안하는 방법",
+    thumbnail: "photo11.png",
+    link: "manual_11.html"
+  }
+];
